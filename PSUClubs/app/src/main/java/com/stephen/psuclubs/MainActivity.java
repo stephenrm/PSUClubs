4a475f57b0d53a4e899ba283456e6601f497d2e3
Mike Chan <michaelchan2018@gmail.com>
2019-03-17 02:06:48 -0400

    Cautious commit bc I'm trying something stupid.  Two buttons still work

--- PSUClubs/app/src/main/java/com/stephen/psuclubs/MainActivity.java
+++ PSUClubs/app/src/main/java/com/stephen/psuclubs/MainActivity.java
@@ -1,11 +1,14 @@
 package com.stephen.psuclubs;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -16,6 +19,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ScrollView scroller = new ScrollView(this);
+//        LinearLayout linLayVar = new LinearLayout(this);
+//
+//        linLayVar.setOrientation(LinearLayout.HORIZONTAL);
+//
+//        scroller.addView(linLayVar);
+//
+//        Button newButt = new Button(this);
+//        newButt.setText("PULLED FROM DATABASE");
+//
+//        LinearLayout.LayoutParams paramsVar = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+//        paramsVar.setMargins(48, 320, 48, 320);
+//        linLayVar.addView(newButt, paramsVar);
+//
+//        this.setContentView(scroller);
     }
 
     public void clubInfo(View v){
