4e3a24b947cfe85151580acd992a5ff8d96dc303
stephenrm <smaransky@comcast.net>
2019-03-17 13:16:18 -0400

    added MyGlobals.sendClubObject() and ClubInfo.receiveClubObject()

--- PSUClubs/app/src/main/java/com/stephen/psuclubs/MyGlobals.java
+++ PSUClubs/app/src/main/java/com/stephen/psuclubs/MyGlobals.java
@@ -9,8 +9,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
@@ -43,6 +45,27 @@ public class MyGlobals {
                             //for (QueryDocumentSnapshot document : task.getResult()) {
                             //    Log.d("testing", document.getId() + " => " + document.getData());
                             //}
+                        } else {
+                            Log.d("testing", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    void sendClubObject(final ClubInfo activity, String name){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a reference to the cities collection
+        CollectionReference clubsRef = db.collection("clubs");
+
+        Query query = clubsRef.whereEqualTo("name", name);
+
+
+        query.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            activity.receiveClubObject((task.getResult().toObjects(Club.class).get(0)));
                         } else {
                             Log.d("testing", "Error getting documents: ", task.getException());
                         }
