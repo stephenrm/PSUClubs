b4149abfa5f13c369df0a082db883294d1f7f9d9
Mike Chan <michaelchan2018@gmail.com>
2019-03-17 13:31:19 -0400

    Information slots update correctly.  Next Step: Color theme

--- PSUClubs/app/src/main/java/com/stephen/psuclubs/ClubInfo.java
+++ PSUClubs/app/src/main/java/com/stephen/psuclubs/ClubInfo.java
@@ -22,13 +22,30 @@ public class ClubInfo extends AppCompatActivity {
 
         TextView titleDisplay = findViewById(R.id.Club_Information);
         titleDisplay.setText(clubTitle);
-//      TextView meetingLocation = findViewById(R.id.meeting_Location);
-//      meetingLocation.setText();
+
+        MyGlobals globalFunctions = new MyGlobals(getApplicationContext());
+        globalFunctions.sendClubObject(this, clubTitle);
 
 
     }
 
     void receiveClubObject(Club receivedClub){
         this.receivedClub=receivedClub;
+
+        TextView meetingLocation = findViewById(R.id.meeting_Location_Details);
+        meetingLocation.setText(receivedClub.getMeetingLocation());
+
+        TextView meetingTime = findViewById(R.id.meeting_Time_Details);
+        meetingTime.setText(receivedClub.getMeetingTimes());
+
+        TextView clubEmail = findViewById(R.id.email_Details);
+        clubEmail.setText(receivedClub.getEmail());
+
+        TextView leadershipDetails = findViewById(R.id.leadership_Details);
+        leadershipDetails.setText(receivedClub.getLeadership());
+
+        TextView description = findViewById(R.id.description_Details);
+        description.setText(receivedClub.getDescription());
+
     }
 }
